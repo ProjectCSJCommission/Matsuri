fa6f867495173b35c6e001c61f57229b3573491e
友人Ｓ <YuuJinS@users.noreply.github.com>
2022-05-30 19:13:35 +0900

    Fix error

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -23,7 +23,7 @@ module.exports = {
 			return message.reply(
 				{
 					allowedMentions: { repliedUser: false },
-					content: `${messageauthor.username}${RandomMorning[Math.floor(Math.random() * RandomMorning.length)]}`,
+					content: `${message.author.username}${RandomMorning[Math.floor(Math.random() * RandomMorning.length)]}`,
 					ephemeral: false,
 				},
 			);
@@ -47,7 +47,7 @@ module.exports = {
 			return message.reply(
 				{
 					allowedMentions: { repliedUser: false },
-					content: `${messageauthor.username}${RandomMorning[Math.floor(Math.random() * RandomMorning.length)]}`,
+					content: `${message.author.username}${RandomMorning[Math.floor(Math.random() * RandomMorning.length)]}`,
 					ephemeral: false,
 				},
 			);
